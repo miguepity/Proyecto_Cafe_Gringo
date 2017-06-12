2862931700785d80e5c5db8779a1de383846f4a9
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-06-12 11:18:14 -0600

    solo para ver la rama 89

---
+++ CafeGringo/src/components/crearEmpleado.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+
+  <div class="container">
+    <h1>hello</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      Nombre:'',
+      Pass:'',
+      date:[],
+      hrIn:[],
+      hrOut:[],
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+  },
+}
+</script>
+
+<style>
+</style>
