08a963b3795f6f7199808d325af07b4bd1ae7588
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-06-16 20:56:34 -0600

    para pasar de rama

--- CafeGringo/src/components/crearEmpleado.vue
+++ CafeGringo/src/components/crearEmpleado.vue
@@ -10,24 +10,24 @@
       </div>
     </nav>
     <div class="row">
-      <div class="col l3 s2 col-izquierda">
+      <div class="col l3 s6 col-izquierda">
         <br>
         <h4 class="nombre">Miguel Ardon</h4>
       </div>
-      <div class="col l9 s2 col-derecha">
+      <div class="col l9 s6">
         <div class="row">
-            <div class="col l8 offset-l2">
+            <div class="col l8  offset-l2">
               <div class="contenido">
                 <div class="row">
-                  <div class="col s5">
-                    <input class="nombres" type="text" placeholder="Nombres" required>
-                    <input class="celular" type="text" placeholder="Celular" required>
-                    <input class="password" type="password" placeholder="Contraseña" required>
+                  <div class="col s6 izquierdo">
+                    <input class="nombreCompleto" type="text" placeholder="  Nombre Completo" required>
+                    <input class="celular" type="text" placeholder="  Celular" required>
+                    <input class="password" type="password" placeholder="  Verificar Contraseña" required>
                   </div>
-                  <div class="col s5">
-                    <input class="username" type="text" placeholder="Username" required>
-                    <input class="email" type="text" placeholder="Correo electronico" required>
-                    <input class="verificarpassword" type="password" placeholder="Verificar Contraseña" required>
+                  <div class="col s6 derecho">
+                    <input class="username" type="text" placeholder="  Username" required>
+                    <input class="email" type="text" placeholder="  Correo electronico" required>
+                    <input class="verificarpassword" type="password" placeholder="  Verificar Contraseña" required>
                   </div>
                   <div class="row">
                     <button class="waves-effect waves-light btn" type="button" name="button">Crear Empleado</button>
@@ -76,22 +76,24 @@
 }
 
 .btn{
-  margin-top: 3%;
-  margin-left: 25%;
+  margin-left: 33%;
+  border-radius: 10px;
+  background-color: #3B8DDF;
 }
 
 .contenido{
-  background-color: rgba(35, 59, 64, 0.6);
+  background-color: #353B40;
   padding-top: 9%;
-  padding-bottom: 3%;
-  padding-left: 6%;
   margin-top: 10%;
+  border-radius: 6px 6px 6px 6px;
 }
 
 input{
-  border: 1px solid white;
-  padding-left: 5%;
+  background-color: white;
   border-radius: 6px 6px 6px 6px;
   margin-bottom: 20%;
+  width: 100%;
+  border:1px solid #DADADA;
 }
+
 </style>
