43af76fd1032656b418ed81645c4e647208bbfd9
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-06-19 20:31:09 -0600

    Agregando fronted

--- src/components/Empleado.vue
+++ src/components/Empleado.vue
@@ -28,20 +28,20 @@
           <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=es&timezone=America%2FTegucigalpa" width="100%" height="130" frameborder="0" seamless></iframe>
 
           <h4>{{bienvenido}} {{userLogin.Nombre}}</h4>
-            <div class="row">
-              <div class="col s4" >
-                <p>Fecha:</p>
-                <p class="date">{{userLogin.date}}</p>
-              </div>
-              <div class="col s4 " >
-                <p>Hora de Entrada:</p>
-                <p  class="start">{{userLogin.hrIn}}</p>
-              </div>
-              <div class="col s4" >
-                <p>Hora de Salida:</p>
-                <p class="finish">{{userLogin.hrOut}}</p>
-              </div>
+          <div class="row">
+            <div class="col s4" >
+              <p>Fecha:</p>
+              <p class="date">{{userLogin.date}}</p>
             </div>
+            <div class="col s4 " >
+              <p>Hora de Entrada:</p>
+              <p  class="start">{{userLogin.hrIn}}</p>
+            </div>
+            <div class="col s4" >
+              <p>Hora de Salida:</p>
+              <p class="finish">{{userLogin.hrOut}}</p>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
           <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat boton" @click="agree">{{textoBoton}}</a>
@@ -74,13 +74,14 @@ export default {
   methods:{
     clickLogin: function(){
       console.log("Entrando!");
-
+      console.log(moment().format());
+      console.log(moment().format("hh:mm"));
       if (this.user.username == "" || this.user.pass == "") {
         sweetAlert({
-            title: "Ohh No!...",
-            text:  "Algo esta mal!...Ingrese Usuario y Contraseña!",
-            type:  "error",
-          });
+          title: "Ohh No!...",
+          text:  "Algo esta mal!...Ingrese Usuario y Contraseña!",
+          type:  "error",
+        });
       }else{
 
 
@@ -119,12 +120,12 @@ export default {
           }else if(res.body.success === false) {
             console.log("entra al else")
             sweetAlert({
-               title: "Ohh No!...",
-               text:  "Algo esta mal!...Usuario o contraseña incorrectos",
-               type:  "error",
-             });
-             $('.username').val("");
-             $('.pass').val("");
+              title: "Ohh No!...",
+              text:  "Algo esta mal!...Usuario o contraseña incorrectos",
+              type:  "error",
+            });
+            $('.username').val("");
+            $('.pass').val("");
           }
         });
 
@@ -132,54 +133,62 @@ export default {
           username: localStorage.getItem("username"),
           date: moment().format()
         }
+        console.log(userGetHoras);
         this.userLogin.date = moment().format("DD-MM-YYYY");
+        var fecha = moment().format();
+        var locals = localStorage.getItem("username");
+        this.$http.get("http://localhost:8000/cafe/gethoras?username="+locals+"&date="+fecha).then((res)=>{
+          console.log(res.body)
 
-        // this.$http.get("http://localhost:8000/cafe/gethoras", userGetHoras).then((res)=>{
-        //   console.log(res.body)
-        //   // this.userLogin.hrIn = moment(res.body.hrIn, "hmm").format("HH:mm");
-        //   // this.userLogin.hrOut = moment(res.body.hrOut, "hmm").format("HH:mm");
-        //  var message = res.body.message;
-        if (message === "entra") {
-          this.textoBoton = "Marcar Entrada"
-        }else if (message === "sale") {
-          this.textoBoton = "Marcar Salida"
-        }
-        // });
-
+          console.log(res);
+          var message = res.body.message;
+          if (message === "entra") {
+            this.textoBoton = "Marcar Entrada"
+          }else if (message === "sale") {
+            this.userLogin.hrIn = moment(res.body.horaEntrada).format("hh:mm");
+            this.textoBoton = "Marcar Salida"
+          }else if(message === "YaSalio"){
+            this.userLogin.hrIn = moment(res.body.horaEntrada).format("hh:mm");
+            this.userLogin.hrOut = moment(res.body.horaSalida).format("hh:mm");
+            this.textoBoton = "Ya Marcó Salida"
+          }
+        });
       }
 
 
     },
     agree:function(){
-      var hrInF = "";
-      var hrOutF = "";
+      var message = "";
+      if (this.textoBoton === "Ya Marcó Salida") {
+        $('.modal').modal('close');
+        $('.username').val("");
+        $('.pass').val("");
+        return;
+      }
       if (this.textoBoton === "Marcar Entrada") {
-        hrInF = moment().format();
-        hrOutF = "nada"
-      }else{
-        hrInF = this.userLogin.hrIn;
-        hrOutF = moment().format();
+        message = "entrada";
+      }else if(this.textoBoton === "Marcar Salida"){
+        message = "salida"
       }
 
       var userEnvio ={
-        date: moment().format(),
         username: localStorage.getItem("username"),
-        hrIn: hrInF,
-        hrOut: hrOutF
+        hora: moment().format(),
+        message: message
       }
-      this.$http.get("http://localhost:8000/cafe/sethoras", userEnvio).then((res)=>{
+      this.$http.put("http://localhost:8000/cafe/marcarhora", userEnvio).then((res)=>{
         if (res.body.success === true) {
           sweetAlert({
-              title: "Perfecto!",
-              text:  "Hora registrada con exito",
-              type:  "success",
-            });
+            title: "Perfecto!",
+            text:  "Hora registrada con exito",
+            type:  "success",
+          });
         }else{
           sweetAlert({
-             title: "Ohh No!...",
-             text:  "Algo esta mal!... Contacta al administrador!",
-             type:  "error",
-           });
+            title: "Ohh No!...",
+            text:  "Algo esta mal!... Contacta al administrador!",
+            type:  "error",
+          });
         }
         $('.modal').modal('close');
         $('.username').val("");
@@ -208,12 +217,12 @@ input[type=text]::-webkit-input-placeholder, input[type=password]::-webkit-input
 }
 
 input[type=text], input[type=password]{
-    width: 100%;
-    padding: 2% 4%;
-    margin: 2% 0;
-    display: inline-block;
-    box-sizing: border-box;
-    background-color: white;
+  width: 100%;
+  padding: 2% 4%;
+  margin: 2% 0;
+  display: inline-block;
+  box-sizing: border-box;
+  background-color: white;
 }
 
 input[type=text]{
@@ -225,30 +234,30 @@ input[type=text]{
 }
 
 .btn{
-    background-color: #4CAF50;
-    color: white;
-    padding: 2% 15%;
-    margin: 2% 0;
-    border: none;
-    cursor: pointer;
-    height: 18%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 2% 15%;
+  margin: 2% 0;
+  border: none;
+  cursor: pointer;
+  height: 18%;
 }
 
 .btn:hover {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 
 .container {
-    background-color: rgba(255,255,255,.60);
-    margin-top: 5%;
-    padding-top: 2%;
-    padding-left: 6%;
-    padding-right: 8%;
-    border-right-width: 5%;
-    padding-bottom: 8%;
-    width: 40%;
-    margin-left: 55%;
-    border-radius: 6px 6px 6px 6px;
+  background-color: rgba(255,255,255,.60);
+  margin-top: 5%;
+  padding-top: 2%;
+  padding-left: 6%;
+  padding-right: 8%;
+  border-right-width: 5%;
+  padding-bottom: 8%;
+  width: 40%;
+  margin-left: 55%;
+  border-radius: 6px 6px 6px 6px;
 }
 
 .pass{
